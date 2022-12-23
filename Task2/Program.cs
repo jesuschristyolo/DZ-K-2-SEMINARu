97bbb809fa161145b45d189c95d5fc3db849b42a
Rustam <valishev57@mail.ru>
2022-12-23 11:35:23 +0500

    complete tasks

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+using static System.Console;
+WriteLine("Введите число");
+int number = Convert.ToInt32(ReadLine());
+if (number < 100)
+{
+    WriteLine("Третьей цифры нет");
+}
+else
+{
+int a = number % 1000;
+int b = a / 100;
+WriteLine(b);
+}
